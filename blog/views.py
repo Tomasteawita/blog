914f7763eb43d3e74c7759148b7baa7e96d8f602
Tomas Cueva <cuevatomass02@gmail.com>
2023-09-01 21:32:00 +0000

    feat(Index)

--- blog/views.py
+++ blog/views.py
@@ -12,9 +12,30 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse, reverse_lazy
 
 class IndexView(View):
+    """
+    Vista basada en clase para la página de inicio.
+    Esta vista muestra una lista de posts y etiquetas únicas relacionadas con los posts.
+    Attributes:
+        template_name (str): El nombre de la plantilla HTML a utilizar para renderizar la página.
+
+    Methods:
+        get(self, request): Procesa una solicitud GET para mostrar la página de inicio.
+    """
     template_name = 'index.html'
 
     def get(self, request):
+        """
+        Procesa una solicitud GET para mostrar la página de inicio.
+        Esta función recibe una solicitud GET, procesa los parámetros de consulta para filtrar
+        posts por etiqueta si se proporciona 'tag_id', y luego renderiza la página de inicio.
+        
+        Args:
+            request (HttpRequest): La solicitud HTTP enviada por el usuario.
+
+        Returns:
+            HttpResponse: Una respuesta HTTP que representa la página de inicio con los posts y
+                          etiquetas únicas relacionadas con los posts.
+        """
         tag_id = request.GET.get('tag_id')
         
         # Obtener todos los tags únicos relacionados con los posts
@@ -26,6 +47,3 @@ class IndexView(View):
             posts = Post.objects.all()
 
         return render(request, self.template_name, {'posts': posts, 'show_tag_buttons': show_tag_buttons})
-
-
-
