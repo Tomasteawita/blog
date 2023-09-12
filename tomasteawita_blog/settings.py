a9d62ae0ddaaf95c90c30852f47ca80620d2cf55
Cueva Tomas Santiago <tomassantiagocueva@gmail.com>
2023-09-12 15:48:12 -0300

    feat styles

--- tomasteawita_blog/settings.py
+++ tomasteawita_blog/settings.py
@@ -30,8 +30,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = secrets.token_hex(32)
 
-DEBUG = True
-#DEBUG = False
+#DEBUG = True
+DEBUG = False
 #SESSION_COOKIE_SECURE = True
 #CSRF_COOKIE_SECURE = True
 #CRSF_TRUSTED_ORIGINS = ['http://tomasteawita.com', 'http://www.tomasteawita.com']
@@ -41,8 +41,8 @@ DEBUG = True
 
 #SECURE_HSTS_SECONDS = 31536000
 #SECURE_SSL_REDIRECT = True
-#ALLOWED_HOSTS = ['tomasteawita.com', 'www.tomasteawita.com', 'localhost', '62.72.24.205']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['tomasteawita.com', 'www.tomasteawita.com', 'localhost', '62.72.24.205']
+#ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -92,30 +92,27 @@ TEMPLATES = [
 WSGI_APPLICATION = 'tomasteawita_blog.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'tomasteawita_blog_db',
-#         'USER': 'tomasteawita',
-#         'PASSWORD': 'TomasteawitaProyecta2002!',
-#         'HOST': 'localhost',
-#         'PORT': '5432'
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres!',
-        'HOST': 'postgre_db',
-        'PORT': '5435'
+        'NAME': 'tomasteawita_blog_db',
+        'USER': 'tomasteawita',
+        'PASSWORD': 'TomasteawitaProyecta2002!',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'postgres',
+#        'USER': 'postgres',
+#        'PASSWORD': 'postgres',
+#        'HOST': 'postgre_db',
+#        'PORT': '5435'
+#    }
+#}
 
 
 # Password validation
